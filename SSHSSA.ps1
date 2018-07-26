a1a58afd5e47a7bf6f65ce10329d7d4ef637cea7
Rayon <rbookal@yahoo.com>
2018-07-26 14:17:36 -0400

    Add files via upload

---
+++ SSHSSA.ps1
@@ -0,0 +1,28 @@
+﻿<#
+    .SYNOPSIS
+        Execute SSACLI using SSH
+    .DESCRIPTION
+    .Author 
+    Rayon Bookal
+#>
+
+if (!(Get-Module -ListAvailable -Name Posh-SSH))
+{
+    Install-Module Posh-SSH
+    Import-Module Posh-SSH
+}
+
+
+$MyCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User #need to create your object with pwd if needed
+#https://blogs.msdn.microsoft.com/koteshb/2010/02/12/powershell-how-to-create-a-pscredential-object/
+
+$SSACMD = "esxcli ssacli cmd -q 'Controller slot=0 array b encode preservedata=yes user=crypto password=xxxx'"
+
+
+    $sshsession = New-SSHSession -ComputerName $ComputerName -Credential $MyCredential
+
+    if ($sshsession.Connected)
+    {
+            $sshsessionresult = Invoke-SSHCommand -SessionId $sshsession.SessionId -Command $SSACMD
+        	Remove-SSHSession -SessionId $sshsession.SessionId 
+}
